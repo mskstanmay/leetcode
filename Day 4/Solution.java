3b4a8bc0f3debe8bd188acebbbf0c1a296e4efbb
mskstanmay <mskstanmay@gmail.com>
2024-03-16 20:57:08 +0530

    Create Solution.java

---
+++ Day 4/Solution.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int findMaxLength(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);  
+        int maxLength = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                count--;
+            } else {
+                count++;
+            }
+
+            if (map.containsKey(count)) {
+                maxLength = Math.max(maxLength, i - map.get(count));
+            } else {
+                map.put(count, i);
+            }
+        }
+
+        return maxLength;
+    }
+}
