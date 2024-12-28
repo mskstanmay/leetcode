3d4500579a499d1a02f23926e05190e783584c29
mskstanmay <mskstanmay@gmail.com>
2024-12-28 22:31:06 +0530

    Time: 89 ms (14.69%) | Memory: 76.9 MB (33.48%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let s = "";
+    while (true) {
+        s += String(head.val);
+        if (head.next === null) {
+            break;
+        }
+        head = head.next;
+    }
+    return s === s.split("").reverse().join("");
+};
