9886364659ad774cf07ea9ab895b4ab913becee6
mskstanmay <mskstanmay@gmail.com>
2025-06-27 13:54:15 +0530

    Time: 1 ms (35.97%) | Memory: 40.8 MB (79.43%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -4,21 +4,10 @@ class Solution {
             return false;
         if (n == 1)
             return true;
-        if ((n % 10 & 1) != 0 || n % 10 == 0)
-            return false;
-
-        while (n % 2 == 0) {
-            n /= 2;
+       
+        while ((n & 1) == 0) {
+            n >>= 1;
         }
-        if (n == 1)
-            return true;
-        else
-            return false;
-
-        // 0,1,2,3,4,5,6,7,8,9
-        /*
-        2 ,4,8,6,2
-        */
-        
+        return n==1;        
     }
 }
