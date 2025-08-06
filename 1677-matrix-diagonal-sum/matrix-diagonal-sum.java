1a1b23cf5f663fda3d3a643c40693b9727946a97
mskstanmay <mskstanmay@gmail.com>
2025-08-06 21:21:58 +0530

    Time: 1 ms (28.52%) | Memory: 44.4 MB (60.71%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -1,7 +1,5 @@
 class Solution {
     public int diagonalSum(int[][] mat) {
-        // 00 11 22  i = j 
-        // i+j = max length so 0 + max , 1+ max-1 so on except  max/2 & max/2
         int res = 0;
         int n = mat.length;
 
