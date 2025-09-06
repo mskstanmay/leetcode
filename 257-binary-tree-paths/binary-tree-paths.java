c664306139f5adb6cce8cbfc3e77d64415b171a4
mskstanmay <mskstanmay@gmail.com>
2025-09-06 21:51:36 +0530

    Time: 1 ms (98.46%) | Memory: 42.6 MB (78.97%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.java
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -18,20 +18,28 @@ class Solution {
         List<String> ans = new ArrayList<>();
         if (root == null)
             return ans;
-        else if (root.left == null && root.right == null) {
-            ans.add("" + root.val);
-            return ans;
-        }
-
-        List<String> left = binaryTreePaths(root.left);
-        List<String> right = binaryTreePaths(root.right);
-
-        for (String s : left) {
-            ans.add(root.val + "->" + s);
-        }
-        for (String s : right) {
-            ans.add(root.val + "->" + s);
-        }
+        dfs(root, new StringBuilder(), ans);
         return ans;
+    }
+
+    private void dfs(TreeNode node, StringBuilder path, List<String> ans) {
+        if (node == null)
+            return;
+
+        int len = path.length();
+        if (path.length() > 0) {
+            path.append("->");
+        }
+        path.append(node.val);
+
+        if (node.left == null && node.right == null) {
+
+            ans.add(path.toString());
+        } else {
+            dfs(node.left, path, ans);
+            dfs(node.right, path, ans);
+        }
+
+        path.setLength(len);
     }
 }
