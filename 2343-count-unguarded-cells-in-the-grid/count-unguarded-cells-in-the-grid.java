c360d374c6d6660c92b2f04fea60ec8378c58e67
mskstanmay <mskstanmay@gmail.com>
2025-11-02 15:30:29 +0530

    Time: 29 ms (29.20%) | Memory: 147.2 MB (5.11%) - LeetSync

---
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
+
+        int[][] matrix = new int[m][n];
+
+        for (int[] wall : walls)
+            matrix[wall[0]][wall[1]] = 1;
+
+        for (int[] guard : guards)
+            matrix[guard[0]][guard[1]] = 2;
+
+        int[][] directionPointers = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+
+        for (int[] guard : guards) {
+            for (int[] direction : directionPointers) {
+                int x = guard[0] + direction[0]; // Moving in directions
+                int y = guard[1] + direction[1]; // Moving in directions
+                while (x >= 0 && x < m && y >= 0 && y < n && matrix[x][y] != 1 && matrix[x][y] != 2) {
+                    if (matrix[x][y] == 0)
+                        matrix[x][y] = 3;
+                    x += direction[0];
+                    y += direction[1];
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (matrix[i][j] == 0)
+                    count++;
+
+        return count;
+    }
+}
