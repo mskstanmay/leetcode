e6eda927379a740e4c820443637688e12d14d43d
mskstanmay <mskstanmay@gmail.com>
2025-11-02 16:30:15 +0530

    Time: 15 ms (19.28%) | Memory: 47.2 MB (5.02%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : magazine.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            if (!map.containsKey(c) || map.get(c) == 0) {
+                return false;
+            }
+            map.put(c, map.get(c) - 1); // Letter Consumed
+        }
+
+        return true;
+    }
+}
