7a7b3d6f3a002ff9373b8f7304cf3397b53d3c6f
mskstanmay <mskstanmay@gmail.com>
2026-01-03 01:07:31 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (12.72%) - LeetSync

---
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        for(int i = 0; i < nums.length - 2; i++){
+            if (nums[i] == nums[i+1] || nums[i] == nums[i+2])
+            return nums[i];}
+            return nums[nums.length -1];
+        
+    }
+}
