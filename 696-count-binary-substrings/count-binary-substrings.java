2705e6d21b6219513ed5b81639676576fa8df55b
mskstanmay <mskstanmay@gmail.com>
2026-02-19 17:13:15 +0530

    Time: 10 ms (87.50%) | Memory: 46.4 MB (72.72%) - LeetSync

--- 696-count-binary-substrings/count-binary-substrings.java
+++ 696-count-binary-substrings/count-binary-substrings.java
@@ -2,6 +2,15 @@ class Solution {
     public int countBinarySubstrings(String s) {
         int res = 0, prev = 0, strk = 1;
 
+/*
+Similar to taking all consecutive streaks of 0 and 1 as sticks 
+and then taking smaller stick as the basis for calculation 
+example
+00 111
+as 00 is 2 digits combinations = 2
+00111
+0111  
+*/
         for (int i = 1; i < s.length(); i++) {
             if (s.charAt(i) == s.charAt(i - 1)) strk++;
             else {
