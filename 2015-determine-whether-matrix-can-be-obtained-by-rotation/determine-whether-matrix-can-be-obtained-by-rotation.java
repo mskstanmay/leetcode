5c4a1edb972e6c373f3f83c725a330c2f84230bd
mskstanmay <mskstanmay@gmail.com>
2026-03-22 15:19:20 +0530

    Time: 1 ms (44.68%) | Memory: 43.9 MB (16.29%) - LeetSync

--- 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -5,10 +5,14 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (mat[i][j] != target[i][j]) m &= 0b1110;
-                if (mat[i][j] != target[j][n - 1 - i]) m &= 0b1101;
-                if (mat[i][j] != target[n - 1 - i][n - 1 - j]) m &= 0b1011;
-                if (mat[i][j] != target[n - 1 - j][i]) m &= 0b0111;
+                if (mat[i][j] != target[i][j])
+                    m &= 0b1110;
+                if (mat[i][j] != target[j][n - 1 - i])
+                    m &= 0b1101;
+                if (mat[i][j] != target[n - 1 - i][n - 1 - j])
+                    m &= 0b1011;
+                if (mat[i][j] != target[n - 1 - j][i])
+                    m &= 0b0111;
 
                 if (m == 0)
                     return false;
