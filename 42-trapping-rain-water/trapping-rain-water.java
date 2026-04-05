1e77816432c5b605dbdf16ca0e5c0dc25cbf8b5b
mskstanmay <mskstanmay@gmail.com>
2026-04-05 09:37:03 +0530

    Time: 1 ms (59.15%) | Memory: 48.7 MB (26.09%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,26 +1,35 @@
+import java.util.*;
 class Solution {
     public int trap(int[] height) {
-        int i = 0, j = height.length - 1;
-        int lmax = 0, rmax = 0;
-        int ans = 0;
-
-        while(i<j){
-            if(height[i] < height[j]){
-                if(height[i] >= lmax){
-                    lmax = height[i];
-                }else {
-                    ans += lmax - height[i];
-                }
-                i++;
-            }else {
-                if(height[j] >= rmax){
-                    rmax = height[j];
-                }else {
-                    ans += rmax - height[j];
-                }
-                j--;
-            }
+        /*
+        Approach 1: Using left side max rod array and right side max rod array
+        Ex: [1,0,2,1,3,2,0,1];
+        lmax = [1,1,2,2,3,3,3,3];
+        rmax = [3,3,3,3,3,2,1,1];
+        for every index:
+            - take min of lmax and rmax at that point
+            - min value - current height => Trapped water 
+        */
+        /*
+        Approach 2: 
+        */
+        int n = height.length;
+        int[] lmax = new int[n];
+        int[] rmax = new int[n];
+        
+        lmax[0] = height[0];
+        rmax[n-1] = height[n-1];
+        for(int i = 1 ;i<height.length;i++){
+            lmax[i] = Math.max(lmax[i-1], height[i]);
         }
-        return ans;
+        for(int i= n-2;i>=0;i--){
+            rmax[i] = Math.max(height[i],rmax[i+1]);
+        }
+        int res = 0;
+        for(int i = 0;i<n;i++){
+            res+=(Math.min(lmax[i],rmax[i]) - height[i]);
+        }
+        return res;
     }
+
 }
