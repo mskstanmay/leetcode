ca65585a7a8c72185523e7ef5c510318428ec3eb
mskstanmay <mskstanmay@gmail.com>
2026-04-05 12:26:10 +0530

    Time: 4 ms (99.35%) | Memory: 103.7 MB (5.33%) - LeetSync

--- 2423-minimum-deletions-to-make-array-divisible/minimum-deletions-to-make-array-divisible.java
+++ 2423-minimum-deletions-to-make-array-divisible/minimum-deletions-to-make-array-divisible.java
@@ -7,15 +7,29 @@ class Solution {
             arraygcd = gcd(arraygcd, x);
         }
 
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (arraygcd % nums[i] == 0)
+        //         return i;
+        // }
+        int min = Integer.MAX_VALUE;
         for (int i = 0; i < nums.length; i++) {
-            if (arraygcd % nums[i] == 0)
-                return i;
+            if (arraygcd % nums[i] == 0) {
+                min = Math.min(min, nums[i]);
+            }
         }
-        return -1;
+        if (min == Integer.MAX_VALUE)
+            return -1;
+        int c = 0;
+        for (int i : nums) {
+
+            if (i < min)
+                c++;
+        }
+        return c;
     }
 
     public int gcd(int a, int b) {
-        return (b==0? a:gcd(b,a%b));
+        return (b == 0 ? a : gcd(b, a % b));
     }
 }
