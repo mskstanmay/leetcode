52159915daf96eccdc5ade643baed8f201fcda72
mskstanmay <mskstanmay@gmail.com>
2026-04-06 12:33:55 +0530

    Time: 47 ms (95.66%) | Memory: 142.1 MB (64.54%) - LeetSync

--- 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.java
+++ 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.java
@@ -1,50 +1,56 @@
-
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 
 class Solution {
     public long kthLargestLevelSum(TreeNode root, int k) {
-        PriorityQueue<Long> pq = new PriorityQueue<>(Collections.reverseOrder());
+        // Use a standard Min-Heap (no reverseOrder) to keep the k largest sums.
+        // The smallest of the top k will stay at the top (peek).
+        PriorityQueue<Long> pq = new PriorityQueue<>();
 
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
+        int levelsCount = 0;
+
         while (!q.isEmpty()) {
             int s = q.size();
             long lvlSum = 0;
             for (int i = 0; i < s; i++) {
                 TreeNode current = q.poll();
-
                 lvlSum += current.val;
 
                 if (current.left != null)
                     q.offer(current.left);
                 if (current.right != null)
                     q.offer(current.right);
-
             }
-            pq.add(lvlSum);
-        }
-
-        if (pq.size() < k)
-            return -1;
-        else {
-            for (int i = 0; i < k - 1; i++) {
+            
+            levelsCount++;
+            pq.offer(lvlSum);
+            
+            // If we have more than k sums, discard the smallest one
+            if (pq.size() > k) {
                 pq.poll();
             }
-            return pq.poll();
         }
+
+        // Standard check: if the tree height is less than k
+        if (levelsCount < k) {
+            return -1;
+        }
+
+        return pq.peek();
     }
 }
