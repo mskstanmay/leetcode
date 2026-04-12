70094be2f56636c798c724455bcd407ab648c6d0
mskstanmay <mskstanmay@gmail.com>
2026-04-12 14:11:48 +0530

    Time: 3 ms (87.75%) | Memory: 42.9 MB (93.98%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty())
+                    return false;
+                char top = stack.pop();
+                if (!matches(top, ch))
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    private boolean matches(char open, char close) {
+        return (open == '(' && close == ')') ||
+                (open == '{' && close == '}') ||
+                (open == '[' && close == ']');
+    }
+}
