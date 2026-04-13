e8572bf43a5f14fc0d181b2fdda3f3620592b0f8
mskstanmay <mskstanmay@gmail.com>
2026-04-13 09:55:35 +0530

    Time: 2 ms (11.89%) | Memory: 46.8 MB (67.27%) - LeetSync

---
+++ 733-flood-fill/flood-fill.java
@@ -0,0 +1,40 @@
+record Point(int x, int y) {
+}
+
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        
+        int originalColor = image[sr][sc];
+        if(color == originalColor ) return image;
+        image[sr][sc] = color;
+        Queue<Point> points = new LinkedList<>();
+        points.add(new Point(sr, sc));
+
+        while (!points.isEmpty()) {
+            Point top = points.poll();
+            // Up, Down, Left, Right
+            int[][] directions = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+
+            for (int[] dir : directions) {
+                int nextX = top.x() + dir[0];
+                int nextY = top.y() + dir[1];
+
+                // 1. Checking boundaries
+                if (nextX >= 0 && nextX < image.length &&
+                        nextY >= 0 && nextY < image[0].length) {
+
+                    // 2. Check if the pixel has same color as original
+                    if (image[nextX][nextY] == originalColor) {
+
+                        // 3. Update color and add to queue
+                        image[nextX][nextY] = color;
+                        points.add(new Point(nextX, nextY));
+                    }
+                }
+            }
+           
+        }
+
+        return image;
+    }
+}
