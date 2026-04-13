90c7c8d963eb127282b58e55c24f0ec50b5d3371
mskstanmay <mskstanmay@gmail.com>
2026-04-13 15:32:55 +0530

    Time: 4 ms (69.17%) | Memory: 94.3 MB (71.29%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,14 +10,15 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
+        /* Approach 1 : Using stack to push and pop all values;
         Stack<Integer> store = new Stack<>();
-
+        
         ListNode start = head;
         while (start.next != null) {
             store.push(start.val);
             start = start.next;
         }
-
+        
         while (head.next != null) {
             int next = store.pop();
             if (head.next.val != next) {
@@ -25,7 +26,42 @@ class Solution {
             }
             head = head.next;
         }
-
+        
         return true;
+        
+        */
+        if (head == null || head.next == null)
+            return true;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode part2 = reverse(slow);
+        ListNode part1 = head;
+
+        while (part2 != null) {
+            if (part1.val != part2.val)
+                return false;
+            part1 = part1.next;
+            part2 = part2.next;
+        }
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
     }
 }
