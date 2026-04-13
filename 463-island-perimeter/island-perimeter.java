52b7d07a6279bca8af3b423f232822c5f19b25e1
mskstanmay <mskstanmay@gmail.com>
2026-04-13 15:51:43 +0530

    Time: 4 ms (100.00%) | Memory: 47.3 MB (40.75%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -1,40 +1,21 @@
 class Solution {
     public int islandPerimeter(int[][] grid) {
-        int rows = grid.length;
-        int cols = grid[0].length;
+        int islands = 0;
+        int neighbours = 0;
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 1) {
-                    // Start DFS as soon as we find any part of the single island
-                    return dfs(grid, i, j);
+                    islands++;
+
+                    if (i > 0 && grid[i - 1][j] == 1)
+                        neighbours++;
+
+                    if (j > 0 && grid[i][j - 1] == 1)
+                        neighbours++;
                 }
             }
         }
-        return 0;
-    }
-
-    private int dfs(int[][] grid, int r, int c) {
-        // If we go out of bounds or hit water, that's an edge (perimeter +1)
-        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == 0) {
-            return 1;
-        }
-        
-        // If we hit a cell we've already visited, it adds nothing to perimeter
-        if (grid[r][c] == -1) {
-            return 0;
-        }
-
-        // Mark the cell as visited
-        grid[r][c] = -1;
-
-        // Sum up perimeter from all 4 directions
-        int perimeter = 0;
-        perimeter += dfs(grid, r + 1, c);
-        perimeter += dfs(grid, r - 1, c);
-        perimeter += dfs(grid, r, c + 1);
-        perimeter += dfs(grid, r, c - 1);
-
-        return perimeter;
+        return islands * 4 - neighbours * 2;
     }
 }
