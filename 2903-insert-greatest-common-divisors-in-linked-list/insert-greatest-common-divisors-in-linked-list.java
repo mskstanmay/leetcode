a17fd1871c2f3463e216aab4627cecd475039905
mskstanmay <mskstanmay@gmail.com>
2026-04-19 14:25:43 +0530

    Time: 1 ms (100.00%) | Memory: 46.9 MB (87.08%) - LeetSync

--- 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.java
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.java
@@ -15,10 +15,9 @@ class Solution {
 
     public ListNode insertGreatestCommonDivisors(ListNode head) {
         ListNode temp = head;
-        int g = temp.val;
-        while (temp.next != null) {
-            g = temp.val;
-            ListNode n = new ListNode(gcd(g, temp.next.val));
+        while (temp != null && temp.next != null) {
+
+            ListNode n = new ListNode(gcd(temp.val, temp.next.val));
             n.next = temp.next;
             temp.next = n;
             temp = n.next;
