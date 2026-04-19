50e8243d1df91158053815fc3a60d2b7b7354467
mskstanmay <mskstanmay@gmail.com>
2026-04-19 12:19:26 +0530

    Time: 6 ms (98.82%) | Memory: 48 MB (11.97%) - LeetSync

--- 1508-longest-happy-prefix/longest-happy-prefix.java
+++ 1508-longest-happy-prefix/longest-happy-prefix.java
@@ -1,5 +1,8 @@
 class Solution {
     public String longestPrefix(String s) {
+        /*
+        Approach1 : two string builders
+        
         int n = s.length();
         StringBuilder prefix = new StringBuilder("");
         StringBuilder suffix = new StringBuilder("");
@@ -9,7 +12,33 @@ class Solution {
             suffix.insert(0, s.charAt(n - 1 - i));
             if(prefix.compareTo(suffix) == 0) res = prefix.toString();
         }
-       // if(prefix.compareTo(suffix) == 0) return prefix.toString();
+        // if(prefix.compareTo(suffix) == 0) return prefix.toString();
         return res;
+        */
+
+        /*Approach 2: 
+        */
+        char c[] = s.toCharArray();
+        int n = s.length();
+        int i = 1;
+        int max = 0;
+
+        int dp[] = new int[n];
+
+        while (i < n) {
+            if (c[max] == c[i]) {
+                max++;
+                dp[i] = max;
+                i++;
+            } else {
+                if (max > 0) {
+                    max = dp[max - 1];
+                } else {
+                    dp[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return s.substring(0, dp[n - 1]);
     }
 }
