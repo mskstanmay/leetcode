5025bcd911ced76daa644a0fcd90279fb3f582c3
mskstanmay <mskstanmay@gmail.com>
2026-05-06 20:34:16 +0530

    Time: 8 ms (59.38%) | Memory: 124.6 MB (86.78%) - LeetSync

---
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -0,0 +1,28 @@
+class Solution {
+    public char[][] rotateTheBox(char[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        //Adjusting them according to gravity
+        for (int r = 0; r < rows; r++) {
+            int p = 0;
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == '.') {
+                    char temp = grid[r][c];
+                    grid[r][c] = grid[r][p];
+                    grid[r][p] = temp;
+                    p++;
+                } else if (grid[r][c] == '*')
+                    p = c + 1;
+            }
+        }
+        
+        //Rotating
+        char[][] res = new char[cols][rows];
+        for (int r = 0; r < rows; r++)
+            for (int c = 0; c < cols; c++)
+                res[c][rows - 1 - r] = grid[r][c];
+                
+        return res;
+    }
+}
