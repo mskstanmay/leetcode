d5def5eb53876e16be9b4a70602cbfa8b304015b
mskstanmay <mskstanmay@gmail.com>
2026-06-26 12:11:43 +0530

    Time: 2 ms (88.85%) | Memory: 48 MB (42.29%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int target = nums.length - 1;
+        for (int i = nums.length-2 ; i >= 0; i--) {
+            if(i + nums[i] >= target) target = i; 
+        }
+        return target==0;
+    }
+}
