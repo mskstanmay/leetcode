c1331ebc087feb2ab9826610a53eff124e3d0dc1
mskstanmay <mskstanmay@gmail.com>
2026-08-09 11:31:08 +0530

    Time: 4 ms (99.74%) | Memory: 81.6 MB (99.64%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,34 +1,51 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
+
         int n = nums.length;
-        Map<Integer, Integer> freq = new HashMap<>();
-        long windowSum = 0;
+
+        int maxValue = 0;
+        for (int x : nums)
+            maxValue = Math.max(maxValue, x);
+
+        int[] freq = new int[maxValue + 1];
+
+        long sum = 0;
         long maxSum = 0;
-        int j = 0;
+        int distinct = 0;
 
-        for (int i = 0; i < n; i++) {
-            // Add the new element
-            int num = nums[i];
-            windowSum += num;
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        // First window
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
 
-            // If window has exceeded size k
-            if (i - j + 1 > k) {
-                int leftNum = nums[j];
-                windowSum -= leftNum;
-                freq.put(leftNum, freq.get(leftNum) - 1);
-                if (freq.get(leftNum) == 0) {
-                    freq.remove(leftNum);
-                }
-                j++;
-            }
+            if (freq[nums[i]] == 0)
+                distinct++;
 
-            // Window size is k, check distinct
-            if (i - j + 1 == k) {
-                if (freq.size() == k) {
-                    maxSum = Math.max(maxSum, windowSum);
-                }
-            }
+            freq[nums[i]]++;
+        }
+
+        if (distinct == k)
+            maxSum = sum;
+
+        // Sliding window
+        for (int i = k; i < n; i++) {
+
+            int left = nums[i - k];
+            freq[left]--;
+            sum -= left;
+
+            if (freq[left] == 0)
+                distinct--;
+
+            int right = nums[i];
+
+            if (freq[right] == 0)
+                distinct++;
+
+            freq[right]++;
+            sum += right;
+
+            if (distinct == k)
+                maxSum = Math.max(maxSum, sum);
         }
 
         return maxSum;
