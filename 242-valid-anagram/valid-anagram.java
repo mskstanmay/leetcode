51d0537cf2cb86f2dcd4d6cb2f7e8755e1813649
mskstanmay <mskstanmay@gmail.com>
2026-08-09 10:19:21 +0530

    Time: 5 ms (61.62%) | Memory: 44.1 MB (90.11%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
+/* HashMAP Approach
         HashMap<Character, Integer> hashmap = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
@@ -25,5 +26,15 @@ class Solution {
 
         }
         return hashmap.size() == 0;
+*/
+
+int[] freq = new int[26];
+
+for(int i = 0 ; i<s.length();i++){
+    freq[s.charAt(i) - 'a']++;
+    freq[t.charAt(i) - 'a']--;
+}
+for(int c : freq) if( c != 0) return false;
+return true;
     }
 }
